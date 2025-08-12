c9425e12131ffe14fe6ab646af27312f395b0cd1
PavelK998 <frost5725@gmail.com>
2025-08-12 22:02:11 +0300

    task-5 ux improvements, apk checksum calculation improved

--- app/src/main/java/ru/pk/testfordrweb/presentation/navigation/SetupNavHost.kt
+++ app/src/main/java/ru/pk/testfordrweb/presentation/navigation/SetupNavHost.kt
@@ -1,11 +1,14 @@
 package ru.pk.testfordrweb.presentation.navigation
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import ru.pk.testfordrweb.R
 import ru.pk.testfordrweb.presentation.screens.installed_app_details.InstalledAppDetailsScreen
 import ru.pk.testfordrweb.presentation.screens.installed_app_details.InstalledAppDetailsUiAction
 import ru.pk.testfordrweb.presentation.screens.installed_app_details.InstalledAppDetailsViewModel
@@ -23,12 +26,21 @@ fun SetupNavHost(navController: NavHostController) {
         composable<Destination.InstalledAppsListScreen> {
             val viewModel = hiltViewModel<InstalledAppsListViewModel>()
             val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
+            val context = LocalContext.current
             ObserveAsActions(viewModel.uiAction) { action ->
-                when(action) {
+                when (action) {
                     is InstalledAppsListUiAction.NavigateToDetailsScreen -> {
                         navController.navigate(
                             Destination.InstalledAppsDetailsScreen(action.packageName)
                         )
+                    }
+
+                    is InstalledAppsListUiAction.ShowError -> {
+                        Toast.makeText(
+                            context,
+                            action.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
@@ -41,10 +53,34 @@ fun SetupNavHost(navController: NavHostController) {
         composable<Destination.InstalledAppsDetailsScreen> {
             val viewModel = hiltViewModel<InstalledAppDetailsViewModel>()
             val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
+            val context = LocalContext.current
             ObserveAsActions(viewModel.uiAction) { action ->
-                when(action) {
+                when (action) {
                     InstalledAppDetailsUiAction.NavigateUp -> {
                         navController.navigateUp()
+                    }
+
+                    is InstalledAppDetailsUiAction.LaunchApp -> {
+                        val packageManager = context.packageManager
+                        val launchIntent =
+                            packageManager.getLaunchIntentForPackage(action.packageName)
+                        if (launchIntent != null) {
+                            context.startActivity(launchIntent)
+                        } else {
+                            Toast.makeText(
+                                context,
+                                context.getString(R.string.start_app_error),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                    is InstalledAppDetailsUiAction.ShowError -> {
+                        Toast.makeText(
+                            context,
+                            action.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
             }
--- app/src/main/java/ru/pk/testfordrweb/presentation/screens/installed_apps_list/InstalledAppsListViewModel.kt
+++ app/src/main/java/ru/pk/testfordrweb/presentation/screens/installed_apps_list/InstalledAppsListViewModel.kt
@@ -9,18 +9,23 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import ru.pk.testfordrweb.R
 import ru.pk.testfordrweb.domain.manager.DeviceManager
+import ru.pk.testfordrweb.domain.manager.ResourceManager
+import ru.pk.testfordrweb.utils.extensions.execute
 import javax.inject.Inject
 
 @HiltViewModel
 class InstalledAppsListViewModel @Inject constructor(
-    private val deviceManager: DeviceManager
+    private val deviceManager: DeviceManager,
+    private val resourceManager: ResourceManager
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(InstalledAppsListState())
     val uiState = _uiState
         .onStart {
-
+            getApplications()
         }
         .stateIn(
             scope = viewModelScope,
@@ -40,4 +45,31 @@ class InstalledAppsListViewModel @Inject constructor(
             }
         }
     }
+
+    private fun getApplications() = viewModelScope.execute(
+        source = {
+            deviceManager.getInstalledAppsDefaultInfo()
+        },
+        onSuccess = { installedApps ->
+            _uiState.update {
+                it.copy(
+                    installedAppsList = installedApps
+                )
+            }
+        },
+        onLoading = { isLoading ->
+            _uiState.update {
+                it.copy(
+                    isLoading = isLoading
+                )
+            }
+        },
+        onError = { throwable ->
+            _uiAction.trySend(
+                InstalledAppsListUiAction.ShowError(
+                    throwable.localizedMessage ?: resourceManager.getString(R.string.error)
+                )
+            )
+        }
+    )
 }
